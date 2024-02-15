390b437c97c7586d5fdd94267b1a73dd487cf1b3
Juan Antonio Rosales Fuentes <jrosales@Juans-MacBook-Pro.local>
2024-02-15 16:20:43 -0600

    fix(api): fix url api

--- src/redux/services/product.ts
+++ src/redux/services/product.ts
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
 export const productApi = createApi({
   reducerPath: 'productApi',
-  baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:3000/api/' }),
+  baseQuery: fetchBaseQuery({ baseUrl: 'https://codifin-test.vercel.app/api/' }),
   endpoints: (builder) => ({
     getProducs: builder.query<IProduct[], {}>({
       query: () => `products/`,
